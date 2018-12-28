e9050e146300dca1f2d591fbd1aec182018f38b3
vineettt <vineetyadav.hacker@gmail.com>
2018-12-28 18:30:02 +0530

    structure

---
+++ application/views/templates/footer.php
@@ -0,0 +1,5 @@
+<script src="<?php echo base_url(); ?>assets/js/jquery-3.3.1.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/main.js"></script>
+</body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+</head>
+<body>
+    
